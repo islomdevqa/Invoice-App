4b96605105c52f66c90b67d3e316532e397ce797
Islomdev16 <jekinsma1604@gmail.com>
2022-06-19 20:54:44 +0500

    Profile model is createed

--- invoice_app/models.py
+++ invoice_app/models.py
@@ -1,5 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-# class Profile(models.Model):
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    account_number = models.CharField(max_length=26, blank=True)
+    company_name = models.CharField(max_length=200)
+    company_info = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f"Profile of: {self.user.username}"
