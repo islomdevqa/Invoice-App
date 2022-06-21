3c95225cc5b899062e28de06242b2cb11de3586e
Islomdev16 <jekinsma1604@gmail.com>
2022-06-21 16:56:25 +0500

    tag column added

--- invoice_app/admin.py
+++ invoice_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Invoice
+from .models import Invoice, Tag
 
 # Register your models here.
 admin.site.register(Invoice)
+admin.site.register(Tag)
--- invoice_app/models.py
+++ invoice_app/models.py
@@ -3,6 +3,12 @@ from profile_app.models import Profile
 from receiver_app.models import Receiver
 
 # Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return str(self.name)
+
 class Invoice(models.Model):
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
     receiver = models.ForeignKey(Receiver, on_delete=models.CASCADE)
@@ -12,6 +18,7 @@ class Invoice(models.Model):
     payment_date = models.DateField()
     created_time = models.DateTimeField(auto_now_add=True)
     closed = models.BooleanField(default=False)
+    tag = models.ManyToManyField(Tag, blank=True)
 
     def __str__(self):
         return f"Invoice number: {self.number}, pk: {self.pk}"
