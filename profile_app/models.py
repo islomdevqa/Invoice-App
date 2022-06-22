cf6023a6965ad2ec7533019b77c9085f3160a055
Islomdev16 <jekinsma1604@gmail.com>
2022-06-22 21:32:53 +0500

    profile model changed

--- profile_app/models.py
+++ profile_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from .utils import generate_account_number
 
 # Create your models here.
 class Profile(models.Model):
@@ -13,6 +14,11 @@ class Profile(models.Model):
     def __str__(self):
         return f"Profile of: {self.user.username}"
 
-    def clean(self):
-        if len(self.account_number) != 12:
-            raise ValidationError("Bank account number must be 12 character long!")
+    def save(self, *args, **kwargs):
+        if self.account_number == "":
+            self.account_number = generate_account_number()
+        return super().save(*args, **kwargs)
+
+    # def clean(self):
+    #     if len(self.account_number) != 12:
+    #         raise ValidationError("Bank account number must be 12 character long!")
--- profile_app/utils.py
+++ profile_app/utils.py
@@ -5,3 +5,5 @@ size = 12
 
 def generate_account_number():
     new_number_list = [str(random.choice(available_numbers)) for _ in range(size)]
+    new_number_str = "".join(new_number_list)
+    return new_number_str
