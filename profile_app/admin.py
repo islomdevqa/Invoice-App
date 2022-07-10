231050ccbfaf43b40a4e699516882155506be981
Islomdev16 <jekinsma1604@gmail.com>
2022-07-10 18:54:45 +0500

    profile admin updated

--- profile_app/admin.py
+++ profile_app/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 from .models import Profile
+from import_export import resources
+from import_export.fields import Field
+from import_export.admin import ExportActionMixin
 
 # Register your models here.
-admin.site.register(Profile)
+class ProfileResource(resources.ModelResource):
+    user = Field()
+    class Meta:
+        model = Profile
+        fields = "__all__"
+
+    def dehydrate_user(self, obj):
+        return obj.user.username
+
+class ProfileAdmin(ExportActionMixin, admin.ModelAdmin):
+    resources_class = ProfileResource
+
+admin.site.register(Profile, ProfileAdmin)
