e74276dbf6a3ebc08661c4489bd786e9ac60306c
Islomdev16 <jekinsma1604@gmail.com>
2022-07-10 18:34:34 +0500

    admin file updated

--- invoice_app/admin.py
+++ invoice_app/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Invoice, Tag
+from import_export import resources
+from import_export.admin import ExportActionMixin
 
 # Register your models here.
+class TagResource(resources.ModelResource):
+    class Meta:
+        model = Tag
+        fields = ('id', 'name')
+
+class TagAdmin(ExportActionMixin, admin.ModelAdmin):
+    resources_class = TagResource
+
 admin.site.register(Invoice)
-admin.site.register(Tag)
+admin.site.register(Tag, TagAdmin)
