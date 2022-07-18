3cc299420de4459f029f173be3e9b241c1cb6feb
Islomdev16 <jekinsma1604@gmail.com>
2022-07-18 06:08:45 +0500

    Invoice app is updates

--- config/views.py
+++ config/views.py
@@ -5,4 +5,8 @@ from invoice_app.models import Invoice
 def hello_world(request):
     obj = Invoice.objects.get(id=1)
     qs = Invoice.objects.all()
-    return render(request, 'home.html')
+    context = {
+        'obj_':obj,
+        'qs':qs,
+    }
+    return render(request, 'home.html', context)
