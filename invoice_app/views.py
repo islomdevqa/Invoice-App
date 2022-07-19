be9fa1d357db12a8ca90276009bd6eef760abf41
Islomdev16 <jekinsma1604@gmail.com>
2022-07-19 05:40:34 +0500

    Invoice views class updated

--- invoice_app/views.py
+++ invoice_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from .models import Invoice
+from profile_app.models import Profile
 
 # Create your views here.
 class InvoiceListView(ListView):
@@ -8,3 +9,10 @@ class InvoiceListView(ListView):
     template_name = "invoice_app/main.html"
     # paginate_by
     context_object_name = 'qs'
+
+    def get_queryset(self):
+        # profile = Profile.objects.get(user=self.request.user)
+        profile = get_object_or_404(Profile, user=self.request.user)
+        qs = Invoice.objects.filter(profile=profile).order_by('-created_time')
+        # return qs
+        return super().get_queryset().filter(profile=profile).order_by('-created_time')
