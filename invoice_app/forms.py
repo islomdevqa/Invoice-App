6dba5e11137c43995c10097a06e9045a2a9e2629
Islomdev16 <jekinsma1604@gmail.com>
2022-07-22 23:54:59 +0500

    Forms widgets and labels are updated

--- invoice_app/forms.py
+++ invoice_app/forms.py
@@ -1,11 +1,22 @@
 from django import forms
 from .models import Invoice
+from django.core.exceptions import ValidationError
 
 class InvoiceForm(forms.ModelForm):
-    completion_date = forms.DateField(widget=forms.DateInput(attrs={'type':'date'}))
-    issue_date = forms.DateField(widget=forms.DateInput(attrs={'type':'date'}))
-    payment_date = forms.DateField(widget=forms.DateInput(attrs={'type':'date'}))
+    completion_date = forms.DateField(widget=forms.DateInput(attrs={'type':'date'}), label="Completion date")
+    issue_date = forms.DateField(widget=forms.DateInput(attrs={'type':'date'}), label="Issue date")
+    payment_date = forms.DateField(widget=forms.DateInput(attrs={'type':'date'}), label="Payment date")
 
     class Meta:
         model = Invoice
         fields = ('receiver', 'number', 'completion_date', 'issue_date', 'payment_date')
+        labels = {
+            "receiver": "Receiver",
+            "number": "Number",
+        }
+
+    def clean_number(self):
+        number = self.cleaned_data.get('number')
+        if len(number) < 10:
+            raise ValidationError("Number is too short !")
+        return number
